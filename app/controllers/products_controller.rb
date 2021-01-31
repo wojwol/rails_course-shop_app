36b558c316d59bcdd3d68e1b11c2ec2e9b0fbf8e
wojwol <woj.woluj@gmail.com>
2021-01-31 21:34:21 +0100

    Made products controller only accept valid params and added form fields to products form file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,6 +64,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.fetch(:product, {})
+      params.require(:product).permit(:name, :quantity, :price)
     end
 end
