f7e2c99e5c5ff8b3c3f618ec1866ca09acca2c47
wojwol <woj.woluj@gmail.com>
2021-02-04 21:43:40 +0100

    Devise installed properly. Added root for routing. Fixed "details" link in products index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :admin do
     resources :products
   end
   resources :products, only: [:index, :show]
+  root "products#index"
 end
