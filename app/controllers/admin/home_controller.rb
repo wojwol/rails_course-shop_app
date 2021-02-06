82623124b096cdbfc6184d18fafc96f71999a089
wojwol <woj.woluj@gmail.com>
2021-02-06 18:53:04 +0100

    made my user 'admin => true' so I can now browse admin endpoints. Fixed 'NoMethodError in Home#index' by adding missing instance variable in home_controller.rb

---
+++ app/controllers/admin/home_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Admin
+  class HomeController < BaseController
+
+    def index
+      @products = Product.all
+      @users_count = User.all.size
+      @products_count = Product.all.size
+      render :index, locals: { users_count: @users_count, products_count: @products_count }
+    end
+  end
+end
